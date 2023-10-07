b1a006dbfa49f5e1db0478e2fbcba155cdb6b5c8
lennikorhonen <lenni.korhonen2@gmail.com>
2023-10-07 18:04:44 +0300

    Colorscheme changes

--- lua/lenni/lazy.lua
+++ lua/lenni/lazy.lua
@@ -101,8 +101,13 @@ require('lazy').setup({
     -- Theme inspired by Atom
     'navarasu/onedark.nvim',
     priority = 1000,
+    opts = {
+      transparent = true,
+    },
     config = function()
       vim.cmd.colorscheme 'onedark'
+      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     end,
   },
 
@@ -166,17 +171,6 @@ require('lazy').setup({
 
   { 'windwp/nvim-autopairs', opts = {} },
 
-  -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
-  --       These are some example plugins that I've included in the kickstart repository.
-  --       Uncomment any of the lines below to enable them.
-  -- require 'kickstart.plugins.autoformat',
-  -- require 'kickstart.plugins.debug',
+  'christoomey/vim-tmux-navigator',
 
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
-  --    up-to-date with whatever is in the kickstart repo.
-  --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  --
-  --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  -- { import = 'custom.plugins' },
 }, {})
